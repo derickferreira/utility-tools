02dcbc62b47e668ffbd9bdf380703930c0d77bc2
derickferreira <derickferreira036@gmail.com>
2024-09-06 07:48:15 +0100

    HTML done

--- box_shadow_react_ts/src/components/BoxShadowGenerator.tsx
+++ box_shadow_react_ts/src/components/BoxShadowGenerator.tsx
@@ -2,23 +2,30 @@
 import { useState, useEffect } from "react";
 
 const BoxShadowGenerator = () => {
-  const [horizontal, setHorizontal] = useState(5);
-  const [vertical, setVertical] = useState(5);
-  const [blur, setBlur] = useState(5);
-  const [spread, setSpread] = useState(5);
-  const [colour, setColour] = useState("#000000");
-  const [opacity, setOpacity] = useState(0.5);
-  const [inset, setInset] = useState(false);
-  const [boxShadow, setBoxShadow] = useState("");
+  const [horizontal, setHorizontal] = useState<number>(5);
+  const [vertical, setVertical] = useState<number>(5);
+  const [blur, setBlur] = useState<number>(5);
+  const [spread, setSpread] = useState<number>(5);
+  const [colour, setColour] = useState<string>("#000000");
+  const [opacity, setOpacity] = useState<number>(0.5);
+  const [inset, setInset] = useState<boolean>(false);
+  const [boxShadow, setBoxShadow] = useState<string>("");
+
+  console.log(opacity);
 
   return (
-    <div id="box_shadow_generator">
-      <div id="controls_container">
+    <div
+      id="box_shadow_generator"
+      className="p-6 bg-white rounded-lg shadow-md"
+    >
+      <div id="controls_container" className="space-y-4">
         <h2 className="text-lg">Box Shadow Settings</h2>
         <div className="form_control">
           <div className="range_input">
             <label htmlFor="horizontal">Horizontal displacement:</label>
             <input
+              onChange={(event) => setHorizontal(parseInt(event.target.value))}
+              value={horizontal}
               type="range"
               name="horizontal"
               id="horizontal"
@@ -28,6 +35,7 @@ const BoxShadowGenerator = () => {
           </div>
           <div className="reference_input">
             <input
+              value={horizontal}
               type="text"
               name="horizontal_value"
               id="horizontal_value"
@@ -40,6 +48,8 @@ const BoxShadowGenerator = () => {
           <div className="range_input">
             <label htmlFor="vertical">Vertical displacement:</label>
             <input
+              onChange={(event) => setVertical(parseInt(event.target.value))}
+              value={vertical}
               type="range"
               name="vertical"
               id="vertical"
@@ -49,6 +59,7 @@ const BoxShadowGenerator = () => {
           </div>
           <div className="reference_input">
             <input
+              value={vertical}
               type="text"
               name="vertical_value"
               id="vertical_value"
@@ -60,49 +71,22 @@ const BoxShadowGenerator = () => {
         <div className="form_control">
           <div className="range_input">
             <label htmlFor="blur">Blur:</label>
-            <input type="range" name="blur" id="blur" min="-75" max="100" />
-          </div>
-          <div className="reference_input">
-            <input type="text" name="blur_value" id="blur_value" readOnly />
-            <p>px</p>
-          </div>
-        </div>
-        <div className="form_control">
-          <div className="range_input">
-            <label htmlFor="spread">Spread:</label>
-            <input type="range" name="spread" id="spread" min="-75" max="100" />
-          </div>
-          <div className="reference_input">
-            <input type="text" name="spread_value" id="spread_value" readOnly />
-            <p>px</p>
-          </div>
-        </div>
-        <div className="form_control">
-          <div className="range_input">
-            <label htmlFor="colour">Colour:</label>
-            <input type="color" name="colour" id="colour" />
-          </div>
-          <div className="reference_input">
-            <input type="text" name="colour_value" id="colour_value" readOnly />
-          </div>
-        </div>
-        <div className="form_control">
-          <div className="range_input">
-            <label htmlFor="opacity">Opacity:</label>
             <input
+              onChange={(event) => setBlur(parseInt(event.target.value))}
+              value={blur}
               type="range"
-              name="opacity"
-              id="opacity"
-              min="0"
-              max="1"
-              step={0.01}
+              name="blur"
+              id="blur"
+              min="-75"
+              max="100"
             />
           </div>
           <div className="reference_input">
             <input
+              value={blur}
               type="text"
-              name="opacity_value"
-              id="opacity_value"
+              name="blur_value"
+              id="blur_value"
               readOnly
             />
             <p>px</p>
@@ -110,25 +94,118 @@ const BoxShadowGenerator = () => {
         </div>
         <div className="form_control">
           <div className="range_input">
-            <label htmlFor="inset">inset shadow? (inset)</label>
-            <input type="range" name="inset" id="inset" min="-75" max="100" />
+            <label htmlFor="spread">Spread:</label>
+            <input
+              onChange={(event) => setSpread(parseInt(event.target.value))}
+              value={spread}
+              type="range"
+              name="spread"
+              id="spread"
+              min="-75"
+              max="100"
+            />
           </div>
           <div className="reference_input">
-            <input type="text" name="inset_value" id="inset_value" readOnly />
+            <input
+              value={spread}
+              type="text"
+              name="spread_value"
+              id="spread_value"
+              readOnly
+            />
             <p>px</p>
           </div>
         </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="colour">Colour:</label>
+            <input
+              onChange={(event) => setColour(event.target.value)}
+              value={colour}
+              type="color"
+              name="colour"
+              id="colour"
+            />
+          </div>
+          <div className="reference_input">
+            <input
+              value={colour}
+              type="text"
+              name="colour_value"
+              id="colour_value"
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="opacity">Opacity:</label>
+            <input
+              onChange={(event) => setOpacity(parseFloat(event.target.value))}
+              value={opacity}
+              type="range"
+              name="opacity"
+              id="opacity"
+              min="0"
+              max="1"
+              step="0.01"
+            />
+          </div>
+          <div className="reference_input">
+            <input
+              value={opacity}
+              type="text"
+              name="opacity_value"
+              id="opacity_value"
+              readOnly
+            />
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="inset">inset shadow? (inset)</label>
+            <input
+              onChange={(event) => setInset(event.target.checked)}
+              checked={inset}
+              type="checkbox"
+              name="inset"
+              id="inset"
+            />
+          </div>
+          <div className="reference_input">
+            <input
+              value={inset ? "with" : "without"}
+              type="text"
+              name="inset_value"
+              id="inset_value"
+              readOnly
+            />
+          </div>
+        </div>
       </div>
-      <div id="preview">
-        <h2>Preview of the generated effect:</h2>
-        <div id="box"></div>
+      <div id="preview" className="flex flex-col items-center">
+        <h2 className="text-xl">Preview of the generated effect:</h2>
+        <div
+          id="box"
+          className="mt-6 w-32 h-32 bg-gray-300 rounded"
+          style={{ boxShadow }}
+        ></div>
       </div>
-      <div id="generated_rule">
-        <h2>Copy and Paste</h2>
-        <div id="rules_area">
-            <div id="rule">box-shadow: <span></span>;</div>
-            <div id="webkit_rule">-webkit-box-shadow: <span></span>;</div>
-            <div id="moz_rule">-moz-box-shadow: <span></span>;</div>
+      <div
+        id="generated_rule"
+        className="mt-6 p-4 bg-white rounded-lg shadow-md"
+      >
+        <h2 className="text-xl">Copy and Paste</h2>
+        <div id="rules_area" className="mt-2">
+          <div id="rule">
+            box-shadow: <span>{boxShadow}</span>;
+          </div>
+          <div id="webkit_rule">
+            -webkit-box-shadow: <span>{boxShadow}</span>;
+          </div>
+          <div id="moz_rule">
+            -moz-box-shadow: <span>{boxShadow}</span>;
+          </div>
         </div>
       </div>
     </div>
