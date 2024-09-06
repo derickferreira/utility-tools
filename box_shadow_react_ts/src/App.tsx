7a15698956e1f3f08b7fb7ca0ac562bd035f064b
derickferreira <derickferreira036@gmail.com>
2024-09-06 07:15:55 +0100

    Controls done

--- box_shadow_react_ts/src/App.tsx
+++ box_shadow_react_ts/src/App.tsx
@@ -1,5 +1,14 @@
+// css
+import "./index.css";
+// components
+import BoxShadowGenerator from "./components/BoxShadowGenerator";
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <BoxShadowGenerator />
+    </div>
+  );
 }
 
 export default App;
--- box_shadow_react_ts/src/components/BoxShadowGenerator.tsx
+++ box_shadow_react_ts/src/components/BoxShadowGenerator.tsx
@@ -1,6 +1,126 @@
+// hooks
+import { useState, useEffect } from "react";
 
 const BoxShadowGenerator = () => {
-  return <div>BoxShadowGenerator</div>;
+  const [horizontal, setHorizontal] = useState(5);
+  const [vertical, setVertical] = useState(5);
+  const [blur, setBlur] = useState(5);
+  const [spread, setSpread] = useState(5);
+  const [colour, setColour] = useState("#000000");
+  const [opacity, setOpacity] = useState(0.5);
+  const [inset, setInset] = useState(false);
+  const [boxShadow, setBoxShadow] = useState("");
+
+  return (
+    <div id="box_shadow_generator">
+      <div id="controls_container">
+        <h2 className="text-lg">Box Shadow Settings</h2>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="horizontal">Horizontal displacement:</label>
+            <input
+              type="range"
+              name="horizontal"
+              id="horizontal"
+              min="-75"
+              max="100"
+            />
+          </div>
+          <div className="reference_input">
+            <input
+              type="text"
+              name="horizontal_value"
+              id="horizontal_value"
+              readOnly
+            />
+            <p>px</p>
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="vertical">Vertical displacement:</label>
+            <input
+              type="range"
+              name="vertical"
+              id="vertical"
+              min="-75"
+              max="100"
+            />
+          </div>
+          <div className="reference_input">
+            <input
+              type="text"
+              name="vertical_value"
+              id="vertical_value"
+              readOnly
+            />
+            <p>px</p>
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="blur">Blur:</label>
+            <input type="range" name="blur" id="blur" min="-75" max="100" />
+          </div>
+          <div className="reference_input">
+            <input type="text" name="blur_value" id="blur_value" readOnly />
+            <p>px</p>
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="spread">Spread:</label>
+            <input type="range" name="spread" id="spread" min="-75" max="100" />
+          </div>
+          <div className="reference_input">
+            <input type="text" name="spread_value" id="spread_value" readOnly />
+            <p>px</p>
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="colour">Colour:</label>
+            <input type="color" name="colour" id="colour" />
+          </div>
+          <div className="reference_input">
+            <input type="text" name="colour_value" id="colour_value" readOnly />
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="opacity">Opacity:</label>
+            <input
+              type="range"
+              name="opacity"
+              id="opacity"
+              min="0"
+              max="1"
+              step={0.01}
+            />
+          </div>
+          <div className="reference_input">
+            <input
+              type="text"
+              name="opacity_value"
+              id="opacity_value"
+              readOnly
+            />
+            <p>px</p>
+          </div>
+        </div>
+        <div className="form_control">
+          <div className="range_input">
+            <label htmlFor="inset">inset shadow? (inset)</label>
+            <input type="range" name="inset" id="inset" min="-75" max="100" />
+          </div>
+          <div className="reference_input">
+            <input type="text" name="inset_value" id="inset_value" readOnly />
+            <p>px</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BoxShadowGenerator;
